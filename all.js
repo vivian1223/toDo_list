e5066b9563ee2b9357f1da8dc43f8ead20d56763
Vivian Yeh <u4h93u6uhu@gmail.com>
2021-08-29 17:51:55 +0800

    Issue: cannot successfully get the data from the created life circle

--- all.js
+++ all.js
@@ -1,23 +1,77 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            apiPath:'https://guarded-hamlet-24255.herokuapp.com/todo6',
-            modalStatus:false,
-            toDoList:[]
-        }
+  data() {
+    return {
+      apiPath: "https://guarded-hamlet-24255.herokuapp.com/todo6",
+      modalStatus: false,
+      toDoList: [],
+      tempToDo: "",
+    };
+  },
+  methods: {
+    showModal() {
+      document.scrollTop = 0;
+      this.modalStatus = true;
     },
-    methods: {
-        addToDo(){
-            this.modalStatus = true;
-            let obj;
-            
+    addToDo() {
+      this.modalStatus = false;
+      if (this.tempToDo == "") {
+        return;
+      }
+      let obj = {};
+      obj.toDo = this.tempToDo;
+      obj.status = "incomplete";
+      axios
+        .post(this.apiPath, obj)
+        .then(function (res) {
+          this.toDoList = res.data;
+          obj = {};
+          window.location.reload();
+        })
+        .catch((err) => console.log(err));
+    },
+    deleteToDo(item) {
+      axios
+        .delete(this.apiPath + `/${item.id}`)
+        .then((res) => {
+          console.log(res);
+          window.location.reload();
+        })
+        .catch((err) => console.log(err));
+    },
+    completeToDo(item) {
+      item.status = "completed";
+      console.log(item);
+      axios
+        .patch(this.apiPath+`/${item.id}`, item)
+        .then((res) => {
+          this.toDoList= res.data;
+          window.location.reload();
+        })
+        .catch((err) => console.log(err));
+    },
+    toDoFilter(status) {
+    //   if (status === "completed") {
+    //     return this.toDoList.filter((item) => (item.status === "completed"));
+    //   }
 
-        }
+      if(status==="incomplete"){
+          console.log(status)
+          return this.toDoList.filter((item) => (item.status === "completed"))
+        };
+      
+      
     },
-    created() {
-        axios.get(this.apiPath)
-        .then(res=>{this.toDoList = res.data;console.log(this.toDoList[1].todo )})
-        .catch(err=>console.log(err))
-    }
-})
-app.mount('#app')
+  },
+  //可以順利取回值，但為什麼不能渲染在畫面上呢？
+  created() {
+    axios
+      .get(this.apiPath)
+      .then((res) => {
+        this.toDoList = res.data;
+       this.toDoFilter('incomplete');
+       console.log(this.toDoFilter('incomplete'))
+      })
+      .catch((err) => console.log(err));
+  },
+});
+app.mount("#app");
