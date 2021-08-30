52f4eef9fdccd96fa5cf3bcc1fd23ad661334149
Vivian Yeh <u4h93u6uhu@gmail.com>
2021-08-30 10:27:32 +0800

    Add edit mode / delete / add toDo function

--- all.js
+++ all.js
@@ -2,23 +2,28 @@ const app = Vue.createApp({
   data() {
     return {
       apiPath: "https://guarded-hamlet-24255.herokuapp.com/todo6",
+      //modal目前是否為開啟
       modalStatus: false,
       toDoList: [],
-      tempToDo: "",
+      //到時候要拿來編輯用的值
+      tempToDo: {},
+      //頁面狀態，用來判斷觸發渲染時，陣列filter會回傳什麼值
+      status: "incomplete",
     };
   },
   methods: {
+    //新增資料時打開 modal
     showModal() {
-      document.scrollTop = 0;
       this.modalStatus = true;
     },
+    //新增todo
     addToDo() {
       this.modalStatus = false;
-      if (this.tempToDo == "") {
+      if (this.tempToDo == {}) {
         return;
       }
       let obj = {};
-      obj.toDo = this.tempToDo;
+      obj.toDo = this.tempToDo.toDo;
       obj.status = "incomplete";
       axios
         .post(this.apiPath, obj)
@@ -29,6 +34,7 @@ const app = Vue.createApp({
         })
         .catch((err) => console.log(err));
     },
+      //刪除todo
     deleteToDo(item) {
       axios
         .delete(this.apiPath + `/${item.id}`)
@@ -38,40 +44,83 @@ const app = Vue.createApp({
         })
         .catch((err) => console.log(err));
     },
+    //完成todo
     completeToDo(item) {
       item.status = "completed";
-      console.log(item);
       axios
-        .patch(this.apiPath+`/${item.id}`, item)
+        .patch(this.apiPath + `/${item.id}`, item)
         .then((res) => {
-          this.toDoList= res.data;
+          this.toDoList = res.data;
           window.location.reload();
         })
         .catch((err) => console.log(err));
     },
-    toDoFilter(status) {
-    //   if (status === "completed") {
-    //     return this.toDoList.filter((item) => (item.status === "completed"));
-    //   }
-
-      if(status==="incomplete"){
-          console.log(status)
-          return this.toDoList.filter((item) => (item.status === "completed"))
-        };
-      
-      
+    //取得資料
+    getData() {
+      axios
+        .get(this.apiPath)
+        .then((res) => {
+          this.toDoList = res.data;
+        })
+        .catch((err) => console.log(err));
+    },
+     //根據點擊內容決定頁面呈現的內容，
+     //由於我們有在 computed 監聽 status 了，
+     //所以當 status 的值有所變動 toDoFilter() 就會回傳新的陣列內容
+    showToDoFilter(status) {
+      switch (status) {
+        case "completed":
+          this.status = "completed";
+          break;
+        case "incomplete":
+          this.status = "incomplete";
+          break;
+        default:
+          break;
+      }
+    },
+    //開啟編輯模式
+    editToDo(toDo){
+    this.status = 'edit';
+     toDo.preStatus = toDo.status;  
+     toDo.status = 'edit';
+     this.tempToDo.toDo = toDo.toDo;   
+    },
+    //完成編輯
+    doneEdit(toDo){
+        toDo.status = toDo.preStatus;
+        console.log(toDo)
+        axios
+        .patch(`${this.apiPath}/${toDo.id}`, this.tempToDo)
+        .then((res) => {
+          this.toDoList = res.data;
+          window.location.reload();
+          this.tempToDo = {};
+        })
+        .catch((err) => console.log(err));
+    }
+  },
+  //透過 computed 來監聽頁面狀況
+  computed: {
+    toDoFilter() {
+      switch (this.status) {
+        case "incomplete":
+          return this.toDoList.filter((item) => item.status === "incomplete");
+          break;
+        case "completed":
+          return this.toDoList.filter((item) => item.status === "completed");
+          break;
+          case "edit":
+            return this.toDoList.filter((item) => item.status === "edit");
+            break;  
+        default:
+          break;
+      }
     },
   },
-  //可以順利取回值，但為什麼不能渲染在畫面上呢？
-  created() {
-    axios
-      .get(this.apiPath)
-      .then((res) => {
-        this.toDoList = res.data;
-       this.toDoFilter('incomplete');
-       console.log(this.toDoFilter('incomplete'))
-      })
-      .catch((err) => console.log(err));
+  // DOM tree ready 時完成取得資料
+  mounted() {
+    this.getData();
   },
 });
 app.mount("#app");
