88e394c8de44ca46e90cac7952fdc8ae297f917d
Vivian Yeh <u4h93u6uhu@gmail.com>
2021-09-03 11:01:52 +0800

    todo list component issue : cannot access global computed

--- all.js
+++ all.js
@@ -100,6 +100,7 @@ const app = Vue.createApp({
   //透過 computed 來監聽頁面狀況
   computed: {
     toDoFilter() {
+      console.log('toDofilter');
       switch (this.status) {
         case "incomplete":
           return this.toDoList.filter((item) => item.status === "incomplete");
@@ -116,7 +117,8 @@ const app = Vue.createApp({
     },
   },
   // DOM tree ready 時完成取得資料
-  mounted() {
+  created() {
+    console.log('App created');
     this.getData();
   },
 });
@@ -128,6 +130,9 @@ app.component("tabComponent", {
   methods: {
     showToDo(status){
       this.$emit('showToDo',status)
+    },
+    getData(){
+      this.$emit('getData')
     }
   },
   template: `<ul class="home-tab mb-3">
@@ -135,4 +140,44 @@ app.component("tabComponent", {
   <li><a href="#" class="home-tab-item" @click.prevent="showToDo('completed')">已完成</a></li>
 </ul>`,
 });
+
+app.component("todoListComponent", {
+  data() {
+    return {
+      toDoList:[]
+    };
+  },
+  props:['todoFilter'],
+  methods: {
+    showToDo(status){
+      this.$emit('showToDo',status)
+    }
+  },
+  created() {
+    console.log('component created');
+  },
+  mounted() {
+    console.log('component mounted');
+  },
+
+  template: `<ul class="position-relative">
+  <li class="home-todo-item" v-for="toDo in toDoFilter" :key="toDo.id">
+      <p v-if="toDo.status != 'edit'">{{toDo.toDo}}</p>
+      <input class="border-primary" v-model="tempToDo.toDo" v-if="toDo.status == 'edit'" type="text">
+      <div class="home-todo-item-edit" v-if="toDo.status != 'edit'">
+          <a @click.prevent="completeToDo(toDo)" href="#" class="link-primary ms-1"><i
+                  class="fas fa-check"></i></a>
+          <a @click.prevent="editToDo(toDo)" href="#" class="link-primary ms-1 fs-2"><i
+                  class="fas fa-pen  fs-2"></i></a>
+          <a @click.prevent="deleteToDo(toDo)" href="#" class="link-primary ms-1 fs-3 "><i
+                  class="fas fa-times"></i></a>
+      </div>
+      <div v-if="toDo.status === 'edit'">
+          <a @click.prevent="doneEdit(toDo)" href="#" class="link-primary ms-1"><i
+              class="fas fa-check"></i></a>
+      </div>
+  </li>
+</ul>`,
+});
+
 app.mount("#app");
